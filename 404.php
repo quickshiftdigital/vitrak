df6bdb65e575d8db27d94e29a9a4a8dcfaa747b0
Umesh Bhalerao <bhaleraoumesh1995@mail.com>
2022-05-27 23:39:55 +0530

    v0.2.0

--- 404.php
+++ 404.php
@@ -1,11 +1,32 @@
-<?php get_header(); ?>
-<article id="post-0" class="post not-found">
-<header class="header">
-<h1 class="entry-title" itemprop="name"><?php esc_html_e( 'Not Found', 'blankslate' ); ?></h1>
-</header>
-<div class="entry-content" itemprop="mainContentOfPage">
-<p><?php esc_html_e( 'Nothing found for the requested page. Try a search instead?', 'blankslate' ); ?></p>
-<?php get_search_form(); ?>
-</div>
-</article>
-<?php get_footer(); ?>
+<?php
+/**
+ * The template for displaying 404 pages (not found)
+ *
+ * @link https://codex.wordpress.org/Creating_an_Error_404_Page
+ *
+ * @package WordPress
+ * @subpackage Ecotech
+ * @since 1.0
+ * @version 1.0
+ */
+
+get_header();
+?>
+    <div id="content" class="container site-content">
+        <div id="primary" class="content-area">
+            <main id="main" class="site-main" role="main">
+                <section class="error-404 not-found">
+                    <h1 class="title"><?php echo esc_html__( 'We are sorry.', 'ecotech' ); ?></h1>
+                    <h3 class="subtitle"><?php echo esc_html__( 'The page you\'ve requested is not available.', 'ecotech' ); ?></h3>
+                    <figure>
+                        <img src="<?php echo get_theme_file_uri( '/assets/images/404.png' ); ?>" alt="404">
+                    </figure>
+                    <a class="button" href="<?php echo esc_url( home_url( '/' ) ); ?>">
+                        <?php echo esc_html__( 'Return to home', 'ecotech' ); ?>
+                    </a>
+                </section><!-- .error-404 -->
+            </main><!-- #main -->
+        </div><!-- #primary -->
+    </div><!-- .wrap -->
+<?php
+get_footer();
--- header.php
+++ header.php
@@ -1,33 +1,34 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Ecotech
+ * @since 1.0
+ * @version 1.0
+ */
 
+?><!DOCTYPE html>
+<html class="no-js" <?php language_attributes(); ?>>
 <head>
-    <meta charset="<?php bloginfo('charset'); ?>" />
-    <meta name="viewport" content="width=device-width" />
+    <meta charset="<?php bloginfo('charset'); ?>"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"/>
+    <link rel="profile" href="https://gmpg.org/xfn/11"/>
+
     <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-    <?php wp_body_open(); ?>
-    <div id="wrapper" class="hfeed">
-        <header id="header" role="banner">
-            <div id="branding">
-                <div id="site-title" itemprop="publisher" itemscope itemtype="https://schema.org/Organization">
-                    <?php
-                    $custom_logo_id = get_theme_mod('custom_logo');
-                    $logo = wp_get_attachment_image_src($custom_logo_id, 'full');
 
-                    if (has_custom_logo()) {
-                        echo '<img src="' . esc_url($logo[0]) . '" alt="' . get_bloginfo('name') . '">';
-                    } else {
-                        echo '<h1>' . get_bloginfo('name') . '</h1>';
-                    }
-                    ?>
-                </div>
-            </div>
-            <nav id="menu" role="navigation" itemscope itemtype="https://schema.org/SiteNavigationElement">
-                <?php wp_nav_menu(array('theme_location' => 'main-menu', 'link_before' => '<span itemprop="name">', 'link_after' => '</span>')); ?>
-            </nav>
-        </header>
-        <div id="container">
-            <main id="content" role="main">
+<?php ecotech_body_open(); ?>
+
+<a href="#" class="overlay-body"></a>
+
+<!-- #page -->
+<div id="page" class="site">
+
+    <?php ecotech_header_template(); ?>
